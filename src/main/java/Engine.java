444405d5b91baec9363d5eee288867f3a36f8d03
Fry98 <filip-toman@seznam.cz>
2019-02-28 12:15:43 +0100

    Wow... a window...

--- src/main/java/Engine.java
+++ src/main/java/Engine.java
@@ -1,23 +1,20 @@
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Engine extends Application {
 
-    @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
-
+  @Override
+  public void start(Stage stage) {
+    Group root = new Group();
+    Scene scene = new Scene(root, 1280, 720);
+    stage.setTitle("TeqEngine");
+    stage.setScene(scene);
+    stage.setResizable(false);
+    stage.show();
+  }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
