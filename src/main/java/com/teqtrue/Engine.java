c717b5958d2efcd68b1d545ba9e6038796b6f492
Fry98 <filip-toman@seznam.cz>
2019-03-02 16:04:13 +0100

    Added ImageScaler + Removed TextureRenderer

--- src/main/java/com/teqtrue/Engine.java
+++ src/main/java/com/teqtrue/Engine.java
@@ -1,8 +1,6 @@
 package com.teqtrue;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -20,7 +18,7 @@ public class Engine extends Application {
   public void start(Stage stage) throws FileNotFoundException {
     stage.setTitle("TeqEngine");
     Group root = new Group();
-    Canvas canvas = new Canvas(1280, 720);
+    Canvas canvas = new Canvas(960, 720);
     GraphicsContext gc = canvas.getGraphicsContext2D();
     drawScreen(gc);
     root.getChildren().add(canvas);
@@ -30,22 +28,14 @@ public class Engine extends Application {
   }
 
   private void drawScreen(GraphicsContext gc) throws FileNotFoundException {
-    Image sprites = new Image(new FileInputStream("src/main/assets/env_day.png"));
-    TextureRenderer ir = new TextureRenderer(gc);
-
-    // RENDERER SETTINGS
-    int tileSize = 16;
-    int scaler = 4;
-    int tilesX = 10;
-    int tilesY = 10;
+    Image sprites = ImageScaler.init("src/main/assets/env_day.png", 4);
 
     // RENDER LOOP
-    for (int i = 0; i < tilesX; i++) {
-      for (int j = 0; j < tilesY; j++) {
-        ir.draw(sprites, 0, 0, tileSize, tileSize, (i*tileSize*scaler), (j*tileSize*scaler), scaler);
-        ir.draw(sprites, (5*tileSize), 0, tileSize, tileSize, (i*tileSize*scaler), (j*tileSize*scaler), scaler);
+    for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < 10; j++) {
+        gc.drawImage(sprites, 0, 0, 64, 64, i*64, j*64, 64, 64);
+        gc.drawImage(sprites, 5*64, 0, 64, 64, i*64, j*64, 64, 64);
       }
     }
-    // gc.drawImage(sprites, 0, 0, 16, 16, 0, 0, 64, 64);
   }
 }
---
+++ src/main/java/com/teqtrue/ImageScaler.java
@@ -0,0 +1,15 @@
+package com.teqtrue;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javafx.scene.image.Image;
+
+public class ImageScaler {
+  public static Image init(String path, int scaler) throws FileNotFoundException {
+    Image source = new Image(new FileInputStream(path));
+    double newWidth = source.getWidth() * scaler;
+    double newHeight = source.getHeight() * scaler;
+    return new Image(new FileInputStream(path), newWidth, newHeight, false, false);
+  }
+}
