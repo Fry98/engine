1a5835804307fca94cb9134169d1cf7591095ab7
Fry98 <filip-toman@seznam.cz>
2019-03-02 12:35:56 +0100

    Added TextureRenderer

---
+++ src/main/java/Core.java
@@ -0,0 +1,5 @@
+public class Core {
+  public void init() {
+    // placeholder
+  }
+}
--- src/main/java/Engine.java
+++ src/main/java/Engine.java
@@ -1,20 +1,49 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Engine extends Application {
+  public static void main(String[] args) {
+    launch(args);
+  }
 
   @Override
-  public void start(Stage stage) {
-    Group root = new Group();
-    Scene scene = new Scene(root, 1280, 720);
+  public void start(Stage stage) throws FileNotFoundException {
     stage.setTitle("TeqEngine");
-    stage.setScene(scene);
+    Group root = new Group();
+    Canvas canvas = new Canvas(1280, 720);
+    GraphicsContext gc = canvas.getGraphicsContext2D();
+    drawScreen(gc);
+    root.getChildren().add(canvas);
+    stage.setScene(new Scene(root));
     stage.setResizable(false);
     stage.show();
   }
-  public static void main(String[] args) {
-    launch(args);
+
+  private void drawScreen(GraphicsContext gc) throws FileNotFoundException {
+    Image sprites = new Image(new FileInputStream("src/main/assets/env_day.png"));
+    TextureRenderer ir = new TextureRenderer(gc);
+
+    // RENDERER SETTINGS
+    int tileSize = 16;
+    int scaler = 3;
+    int tilesX = 10;
+    int tilesY = 10;
+
+    // RENDER LOOP
+    for (int i = 0; i < tilesX; i++) {
+      for (int j = 0; j < tilesY; j++) {
+        ir.draw(sprites, 0, 0, tileSize, tileSize, (i*tileSize*scaler), (j*tileSize*scaler), scaler);
+        ir.draw(sprites, (5*tileSize), 0, tileSize, tileSize, (i*tileSize*scaler), (j*tileSize*scaler), scaler);
+      }
+    }
+    // gc.drawImage(sprites, 0, 0, 16, 16, 0, 0, 64, 64);
   }
 }
